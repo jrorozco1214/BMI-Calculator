18484de42cf27e07fc118deb7681bf6049b4cbc6
Jeremy <jeremyorozco38@gmail.com>
2025-07-29 21:28:11 -0500

    changes

--- serverPy.py
+++ serverPy.py
@@ -3,6 +3,7 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
+#constants
 FEET_CONVERSION = 12
 FORMULA_CONSTANT = 703
 
@@ -13,14 +14,29 @@ def BMICalculations():
         Inches = int(request.form.get("Inches"))
         Pounds = float(request.form.get("Pounds"))
 
-        result = BMICalculator(Feet, Inches, Pounds)
-        return render_template("BMI_home.html", result = result)
+        total = BMICalculatorFun(Feet, Inches, Pounds)
+        stringResults = StringDeterminedResults(total)
+
+        return render_template("BMI_home.html", result = total, categoryResult = stringResults) 
+    # when you are returning these variables they need to be established in the html first 
     else:
         return render_template("BMI_home.html")
 
+# function used in order to calculate BMI
+def BMICalculatorFun(feet, inches, pounds):
+    feet_converted = ((feet * FEET_CONVERSION) + inches)
+    pre = (pounds*FORMULA_CONSTANT) / feet_converted
+    return float(format((pre / feet_converted), ".1f"))
 
-def BMICalculator(feet, inches, pounds):
-    return format(FORMULA_CONSTANT * (pounds / ((feet * FEET_CONVERSION) + inches)), ".2f")
+def StringDeterminedResults(calculatedTotal):
+
+    if(calculatedTotal < 18.5):
+        return "Underweight"
+    elif(calculatedTotal >= 18.5 and calculatedTotal <= 24.9):
+        return "Healthy"
+    elif(calculatedTotal >= 25.0 and calculatedTotal <= 29.9):
+        return "Overweight"
+    return "Obesity"
 
 if __name__=="__main__":
     app.run(host="0.0.0.0")
